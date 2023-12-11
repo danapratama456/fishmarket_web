cd8ede01510d8a3db111daa3fbeb4cbf5a2dd177
DESKTOP-OAEFTSO\Administrator <andyfebri742@gmail.com>
2023-12-11 19:19:38 +0700

    first commit

---
+++ database/migrations/2023_12_09_071324_create_transactions_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_user');
+            $table->integer('id_table');
+            $table->text('notes');
+            $table->integer('total');
+            $table->integer('subtotal');
+            $table->integer('tax');
+            $table->string('status');
+            $table->boolean('is_deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
